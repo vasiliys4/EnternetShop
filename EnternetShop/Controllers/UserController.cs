0f07fc5d504ff81815e00ede0a86a6d9d84b4a52
Сентемов Василий <sentemovv55@gmail.com>
2023-07-28 16:08:03 +0400

    добавлена работа с аккаунтом

---
+++ EnternetShop/Controllers/UserController.cs
@@ -0,0 +1,98 @@
+﻿using EnternetShop.Extention;
+using EnternetShop.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnternetShop.Controllers
+{
+    [Authorize]
+    public class UserController : Controller
+    {
+        private readonly UserManager<UserForDB> _userManager;
+        public UserController(UserManager<UserForDB> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.FindByIdAsync(_userManager.GetUserId(User));
+            var model = new User { Id = user.Id, Email = user.Email, Name = user.FirstName, Surname = user.LastName, PhoneNumber = user.PhoneNumber };
+            return View(model);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Edit(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            var model = new User { Id = user.Id, Email = user.Email, Name = user.FirstName, Surname = user.LastName, PhoneNumber = user.PhoneNumber };
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(User model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByIdAsync(model.Id);
+                if (user != null)
+                {
+                    user.Email = model.Email;
+                    user.UserName = model.Email;
+                    user.FirstName = model.Name;
+                    user.LastName = model.Surname;
+                    user.PhoneNumber = model.PhoneNumber;
+                    var result = await _userManager.UpdateAsync(user);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        result.AddErrorsTo(ModelState);
+                    }
+                }
+            }
+            return View(model);
+        }
+        [HttpGet]
+        public async Task<IActionResult> ChangePassword(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            var model = new User { Id = user.Id, Email = user.Email, Name = user.FirstName };
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> ChangePassword(User model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByIdAsync(model.Id);
+                if (user != null)
+                {
+                    var result = await _userManager.ChangePasswordAsync(user, model.Password, model.NewPassword);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        result.AddErrorsTo(ModelState);
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Пользователь не найден");
+                }
+            }
+            return View(model);
+        }
+    }
+}
--- EnternetShop/Models/User.cs
+++ EnternetShop/Models/User.cs
@@ -4,9 +4,12 @@ namespace EnternetShop.Models
 {
     public class User
     {
-        public Guid Id { get; set; }
+        public string? Id { get; set; }
         public string? Email { get; set; }
         public string? Password { get; set; }
+        public string? NewPassword { get; set; }
+        [Compare("NewPassword", ErrorMessage = "Пароли не совпадают")]
+        public string? PasswordConfirm { get; set; }
         public string? Name { get; set; }
         public string? Surname { get; set; }
         public string? PhoneNumber { get; set; }
